d5c7344dfceed56e7cc0cdee874b4fcdbe87252b
jonathan machado <jonathan.machado@microsoft.com>
2023-01-27 16:57:10 +0100

    init

--- source/conf.py
+++ source/conf.py
@@ -16,7 +16,7 @@ release = '1.0.0'
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ['sphinx_rtd_theme']
 exclude_patterns = []
 
 
@@ -24,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
